0d99c7ade973a82b9ad3634a1f60f7148aff2db9
tina ! <144720388+tinaduo@users.noreply.github.com>
2024-04-22 13:02:49 -0700

    goorve

--- pages/chart.js
+++ pages/chart.js
@@ -14,6 +14,7 @@ export default function Chart() {
       <main className={styles.main} >
         <h1 className={`${styles.grooveHeader}`}>GROOVE</h1>
         <p className={`${styles.paragraph}`}>Get ready to groove through the decades with our chart showcasing the top music genres that have shaped the soundtracks of our lives. From the swinging jazz of the 1920s to the rock 'n' roll revolution of the 1950s, and from the disco fever of the 1970s to the digital beats of the 21st century, explore how music has evolved and enchanted audiences across generations.</p>
+        <h3 className={`${styles.grooveHeader}`}>Top 3 Music Genres</h3>
         <PieChart/>
         <p className={`${styles.paragraph}`}>Whether you're vibing to the soulful melodies of Motown or getting down to the infectious rhythms of hip-hop, our chart highlights the timeless hits that have stood the test of time and continue to resonate with music lovers worldwide. So tune in, and let the music take you on your own journey through the greatest genres of all time with Groove.</p>
       </main>
