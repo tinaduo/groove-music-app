2298f3a32b2e8bde229cf6604436348dcaba3f71
tina ! <144720388+tinaduo@users.noreply.github.com>
2024-04-23 11:05:18 -0700

    button

---
+++ components/Button/index.js
@@ -0,0 +1,10 @@
+import styles from './ProgressBar.module.css';
+
+export function tuneboxButton() {
+    return (
+      <div className={styles.tuneboxButton}>
+        <p className={styles.buttonText}>Go to Tunebox</p>
+      </div>
+    );
+  }
+  
