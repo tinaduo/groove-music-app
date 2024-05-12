5d6b60e0489ec6a467a3acf1dae221426767a4a9
tinaduo <144720388+tinaduo@users.noreply.github.com>
2024-05-11 23:14:40 -0700

    creating

--- pages/TokenContext.js
+++ pages/TokenContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
 
 const TokenContext = createContext();
