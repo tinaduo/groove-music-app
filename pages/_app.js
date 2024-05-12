8fdbffe4dcc387fce226ae7afa6b792e0662d5f4
tinaduo <144720388+tinaduo@users.noreply.github.com>
2024-05-12 00:19:40 -0700

    REVERT

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,5 @@
-import React, { createContext, useState, useContext } from 'react';
+import "@/styles/globals.css";
 
-const SpotifyContext = createContext();
-
-export const SpotifyProvider = ({ children }) => {
-  const [accessToken, setAccessToken] = useState('');
-
-  return (
-    <SpotifyContext.Provider value={{ accessToken, setAccessToken }}>
-      {children}
-    </SpotifyContext.Provider>
-  );
-};
-
-export const useSpotify = () => useContext(SpotifyContext);
-
-function App({ Component, pageProps }) {
-  return (
-    <SpotifyProvider>
-      <Component {...pageProps} />
-    </SpotifyProvider>
-  );
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default App;
