73c7bd26a98c07fdf6b25fa49d8093cdf1b5d123
tinaduo <144720388+tinaduo@users.noreply.github.com>
2024-05-12 00:25:27 -0700

    token testing

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import "@/styles/globals.css";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { getSessionToken } from '../utils/auth';
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = getSessionToken();
+    if (!token && router.pathname !== '/login') {
+      router.push('/login');
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
+
+export default App;
