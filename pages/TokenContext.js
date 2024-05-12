7a6c4c1bc9f081aac35921e42e4ce8ec903853f0
tinaduo <144720388+tinaduo@users.noreply.github.com>
2024-05-11 23:17:00 -0700

    oops

--- pages/TokenContext.js
+++ pages/TokenContext.js
@@ -31,36 +31,3 @@ export const TokenProvider = ({ children }) => {
 };
 
 export default TokenContext;
-import React, { createContext, useState, useEffect } from 'react';
-import Cookies from 'js-cookie';
-
-const TokenContext = createContext();
-
-export const TokenProvider = ({ children }) => {
-    const [token, setToken] = useState('');
-
-    useEffect(() => {
-        const accessTokenFromCookie = Cookies.get('accessToken');
-        if (accessTokenFromCookie) {
-            setToken(accessTokenFromCookie);
-        }
-    }, []);
-
-    const handleSetToken = (newToken) => {
-        setToken(newToken);
-        Cookies.set('accessToken', newToken);
-    };
-
-    const handleRemoveToken = () => {
-        setToken('');
-        Cookies.remove('accessToken');
-    };
-
-    return (
-        <TokenContext.Provider value={{ token, setToken: handleSetToken, removeToken: handleRemoveToken }}>
-            {children}
-        </TokenContext.Provider>
-    );
-};
-
-export default TokenContext;
