310a7015dcef8601ea2386c0f65928a36348d787
tinaduo <144720388+tinaduo@users.noreply.github.com>
2024-05-13 00:51:37 -0700

    redoing

--- components/PostForm/index.js
+++ components/PostForm/index.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
-import styles from "./PostForm.module.css";
+import { useHistory } from 'react-router-dom';
+import styles from './PostForm.module.css';
 import StarRating from '../StarRating';
-
+import Snackbar from '../SnackBar';
 
 export function PostForm() {
     const [title, setTitle] = useState('');
     const [review, setReview] = useState('');
     const [date, setDate] = useState('');
+    const [showSnackbar, setShowSnackbar] = useState(false);
+    const [snackbarMessage, setSnackbarMessage] = useState('');
+
+    const history = useHistory();
 
     const handleTitleChange = (event) => {
         setTitle(event.target.value);
@@ -20,8 +25,13 @@ export function PostForm() {
         setDate(event.target.value);
     };
 
-    const goToExplorePage = () => {
-    window.location.href = '/Explore';
+    const displaySnackbar = (message) => { // Function to display the Snackbar
+        setSnackbarMessage(message);
+        setShowSnackbar(true);
+        setTimeout(() => {
+            setShowSnackbar(false);
+            setSnackbarMessage('');
+        }, 3000);
     };
 
     const handleSubmit = (event) => {
@@ -32,88 +42,93 @@ export function PostForm() {
         setTitle('');
         setReview('');
         setDate('');
+        history.push('/explore');
+        displaySnackbar('Review posted successfully!'); // Call the displaySnackbar function
     };
 
     return (
-        <form className={styles.postForm} onSubmit={handleSubmit}>
-            <div className={styles.container}>
-                <label
-                    className={styles.headers}
-                    for="rating">Rating</label>
-                <br />
-                <StarRating/>
-                <label
-                    className={styles.headers}
-                    for="title">Title</label>
-                <br />
-                <input
-                    className={styles.titleTextbox}
-                    type="text"
-                    id="title"
-                    value={title}
-                    placeholder='review title'
-                    onChange={handleTitleChange}
-                    required />
-                <br />
-            </div>
-            <div className={styles.textboxContainer}>
-                <br />
-                <textarea
-                    className={styles.textarea}
-                    id="review"
-                    value={review}
-                    onChange={handleReviewChange}
-                    placeholder='write a review...'
-                    rows="10"
-                    cols="50"
-                    maxLength="2500"
-                    required />
-                <br />
-                <span
-                    className={styles.subtitle}>
-                    Maximum: {2500 - review.length}/2500
-                </span>
-            </div>
-            <div className={styles.inputscontainer}>
-                <div className={styles.dateContainer}>
+        <div>
+            <form className={styles.postForm} onSubmit={handleSubmit}>
+                <div className={styles.container}>
                     <label
                         className={styles.headers}
-                        for="date">
-                        Listened on
-                    </label>
+                        htmlFor="rating">Rating</label>
+                    <br />
+                    <StarRating/>
+                    <label
+                        className={styles.headers}
+                        htmlFor="title">Title</label>
                     <br />
                     <input
-                        className={styles.datePicker}
-                        type="date"
-                        id="date"
-                        value={date}
-                        onChange={handleDateChange}
+                        className={styles.titleTextbox}
+                        type="text"
+                        id="title"
+                        value={title}
+                        placeholder='review title'
+                        onChange={handleTitleChange}
                         required />
                     <br />
                 </div>
-                <div className={styles.checkboxContainer}>
-                    <h3 className={styles.headers}>Listen Again?</h3>
-                    <div className={styles.inputcheckboxContainer}>
-                        <input
-                            className={styles.checkbox}
-                            type="checkbox"
-                            id="relisten"
-                            name="relisten"
-                        />
+                <div className={styles.textboxContainer}>
+                    <br />
+                    <textarea
+                        className={styles.textarea}
+                        id="review"
+                        value={review}
+                        onChange={handleReviewChange}
+                        placeholder='write a review...'
+                        rows="10"
+                        cols="50"
+                        maxLength="2500"
+                        required />
+                    <br />
+                    <span
+                        className={styles.subtitle}>
+                        Maximum: {2500 - review.length}/2500
+                    </span>
+                </div>
+                <div className={styles.inputscontainer}>
+                    <div className={styles.dateContainer}>
                         <label
-                            className={styles.subtitleCheckbox}
-                            for="relisten"
-                        >
-                            Mark as Re-listen
+                            className={styles.headers}
+                            htmlFor="date">
+                            Listened on
                         </label>
+                        <br />
+                        <input
+                            className={styles.datePicker}
+                            type="date"
+                            id="date"
+                            value={date}
+                            onChange={handleDateChange}
+                            required />
+                        <br />
+                    </div>
+                    <div className={styles.checkboxContainer}>
+                        <h3 className={styles.headers}>Listen Again?</h3>
+                        <div className={styles.inputcheckboxContainer}>
+                            <input
+                                className={styles.checkbox}
+                                type="checkbox"
+                                id="relisten"
+                                name="relisten"
+                            />
+                            <label
+                                className={styles.subtitleCheckbox}
+                                htmlFor="relisten"
+                            >
+                                Mark as Re-listen
+                            </label>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <button
-                className={styles.submitButton}
-                onClick={goToExplorePage}>
-                Post Review
-            </button>
-        </form>
+                <button
+                    className={styles.submitButton}
+                    type="submit">
+                    Post Review
+                </button>
+            </form>
+            {showSnackbar && <Snackbar message={snackbarMessage} onClose={() => setShowSnackbar(false)} />}
+        </div>
     );
 }
