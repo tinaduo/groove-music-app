ed6bcd09f48b319fe3ed8c918eaaa1e08eab1af2
tinaduo <144720388+tinaduo@users.noreply.github.com>
2024-05-12 22:43:22 -0700

    test

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,31 @@ import { useSession } from 'next-auth/react';
 import LoginButton from '@/components/LoginButton';
 
 export default function Home() {
-  const { data: session } = useSession();
+  const session = useSession();
 
-  return (
-    <div>
-      {session ? (
-        <div>
-          <p>Welcome, {session.user.name}!</p>
-          {/* Your authenticated content */}
-        </div>
-      ) : (
-        <div>
-          <p>You are not logged in.</p>
-          <LoginButton />
-        </div>
-      )}
-    </div>
-  );
+  if (!session) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  // if session is defined, proceed to check if user is logged in
+  if (session.data) {
+    return (
+      <div>
+        <p>Welcome, {session.data.user.name}!</p>
+        { /* woooo */ }
+      </div>
+    );
+  } else {
+    // user is not logged in
+    return (
+      <div>
+        <p>You are not logged in.</p>
+        <LoginButton />
+      </div>
+    );
+  }
 }
