9f5ef27201c4c1a24e180fe412758085773834ab
tinaduo <144720388+tinaduo@users.noreply.github.com>
2024-05-12 22:03:04 -0700

    test

--- components/NavigationBar/index.js
+++ components/NavigationBar/index.js
@@ -2,7 +2,31 @@ import Image from 'next/image';
 import styles from './NavBar.module.css';
 import Link from 'next/link';
 
+const CLIENT_ID = "22ed42382ae44ed69f8d3a6da1c6e077"
+const REDIRECT_URI = "https://groove-music-app.vercel.app/newreview"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const RESPONSE_TYPE = "token"
+
 export default function NavBar() {
+
+      useEffect(() => {
+        const storedToken = sessionStorage.getItem("spotifyToken");
+        if (storedToken) {
+            setToken(storedToken);
+        } else {
+            const hashParams = window.location.hash.substr(1).split("&").reduce(function (result, item) {
+                const parts = item.split("=");
+                result[parts[0]] = parts[1];
+                return result;
+            }, {});
+
+            if (hashParams.access_token) {
+                sessionStorage.setItem("spotifyToken", hashParams.access_token);
+                setToken(hashParams.access_token);
+            }
+        }
+    }, []);
+
   return (
     <header className={styles.header}>
       <ul className={styles.list}>
@@ -23,7 +47,8 @@ export default function NavBar() {
           </Link>
         </li>
         <li>
-          <Link href="/newreview" className={styles.text}>
+        {!token ?
+          <Link href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`} className={styles.text}>
             <div className={styles.link}>
               <Image src="/images/newPost.svg" alt="New Post" width={30} height={30} />
               <span>New Post</span>
