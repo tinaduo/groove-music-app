68ebf753ebc4bd116098b024e08a052331560bb9
tinaduo <144720388+tinaduo@users.noreply.github.com>
2024-05-13 00:19:08 -0700

    postcard, delete login

--- components/LoginButton/index.js
+++
@@ -1,9 +0,0 @@
-import { signIn } from 'next-auth/react';
-
-export default function LoginButton() {
-  return (
-    <button onClick={() => signIn('spotify')}>
-      Login with Spotify
-    </button>
-  );
-}
